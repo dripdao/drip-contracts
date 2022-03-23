7e2b953a870f423dbcb1016ff1f73c6dbdc1acf2
dripDAO <dripbonddao@gmail.com>
2022-03-23 11:54:16 -0500

    update

--- deploy/1_deploy.js
+++ deploy/1_deploy.js
@@ -2,12 +2,12 @@
 
 const { deployments, upgrades, ethers } = require('hardhat');
 const gasnow = require('ethers-gasnow');
-ethers.providers.BaseProvider.prototype.getGasPrice = async () => ethers.utils.parseUnits('16', 9);
+ethers.providers.BaseProvider.prototype.getGasPrice = async () => ethers.utils.parseUnits('25', 9);
 
 module.exports = async () => {
   const [ signer ] = await ethers.getSigners();
-  const tx = await deployments.deploy('DRIP', {
-    contractName: 'DRIP',
+  const tx = await deployments.deploy('DRIPBOND', {
+    contractName: 'DRIPBOND',
     args: [],
     libraries: {},
     from: await signer.getAddress()
--- test/test.js
+++ test/test.js
@@ -8,19 +8,21 @@ describe('drip-contracts', () => {
   before(async () => {
     await hre.deployments.fixture();
   });
-  it('DRIP.sol', async () => {
-    const drip = await hre.ethers.getContract('DRIP');
-    await drip.initialize();
-  });
   it('DRIPBOND.sol', async () => {
-    const drip = new ethers.Contract('0x0d44CfA6a50E4C16eE311af6EDAD36E89f90b0a6', [ 'function permission(address, address, uint256)' ], (await ethers.getSigners())[0])
+    const [ signer ] = await ethers.getSigners();
+    const drip = new ethers.Contract('0x0d44CfA6a50E4C16eE311af6EDAD36E89f90b0a6', [ 'function balanceOf(address) view returns (uint256)', 'function permission(address, address, uint256)' ], signer);
     const dripMockArtifact = require('../artifacts/contracts/test/DRIPMock.sol/DRIPMock');
     await provider.send('hardhat_setCode', [ethers.utils.getAddress(drip.address), dripMockArtifact.deployedBytecode ]);
     const { address: treasuryAddress } = require('../deployments/mainnet/GnosisSafe');
-    const dripBond = await (await hre.ethers.getContractFactory('DRIPBOND')).deploy();
+    const dripBondProxy = await (await hre.ethers.getContractFactory('DRIPBONDProxy')).deploy();
+    const dripBond = new ethers.Contract(dripBondProxy.address, [ 'function mint() payable returns (uint256)', 'function burn(uint256)' ], dripBondProxy.signer);
     await drip.permission(treasuryAddress, dripBond.address, ethers.constants.MaxUint256);
-    await dripBond.initialize();
+//    await dripBond.initialize();
 
     const tx = await dripBond.mint({ value: ethers.utils.parseEther('1') });
+    await provider.send('evm_setNextBlockTimestamp', [Math.floor(Date.now() / 1000) + 60*60*24*60]);
+    await provider.send('evm_mine');
+    await dripBond.burn(0);
+    console.log(ethers.utils.formatEther(await drip.balanceOf(await signer.getAddress())));
   });
 });
